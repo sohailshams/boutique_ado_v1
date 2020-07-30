f29950cc1280451e0bccabb0e5cbdbd7f50d6c02
Sohail <sohailshamas@yahoo.com>
2020-07-30 13:33:23 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HAY1mEIrdLElwwaC6GD6C2LilP19yHEibeqrffIUYbyYCaInxY6r6KzjIBjncHRxUKzQ3SbYEb5em9IdBKPzaYI00OlgLT2a4',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
