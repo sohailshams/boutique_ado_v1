65beb991e756cb70719a1feb50a81cff018a1f72
Sohail <sohailshamas@yahoo.com>
2020-08-09 13:23:34 +0000

    046 profiles part and completed email confirmation

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -32,7 +32,7 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-    
+
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
