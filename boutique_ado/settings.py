3bbc8d4d58e611562dd42ae0ceeb753871033497
Sohail <sohailshamas@yahoo.com>
2020-08-12 09:40:25 +0000

    added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -176,7 +176,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket Config for AWS
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket Config 
     AWS_STORAGE_BUCKET_NAME = 'mss-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
